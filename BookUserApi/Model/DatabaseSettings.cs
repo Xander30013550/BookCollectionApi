bc18d709fba0450df4c521e3fa503e63720a7184
Xander30013550 <163367498+Xander30013550@users.noreply.github.com>
2025-05-16 15:39:38 +0800

    Delete BookUserApi directory

--- BookUserApi/Model/DatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookUserApi.Model
-{
-    public class DatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string UsersCollectionName { get; set; } = null!;
-    }
-}
